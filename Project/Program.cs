3e63973b2bf29ecb8dd683e5701621ed0abdcdb4
keseru.marton@stud.uni-obuda.hu <keseru.marton@stud.uni-obuda.hu>
2024-12-02 12:53:13 +0100

    kész1.0

--- Project/Program.cs
+++ Project/Program.cs
@@ -66,10 +66,7 @@ namespace Project
            
         }
 
-        private static void SomeAction()
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public static void DisplaySalaryBarChart(List<Employee> employees)
         {
@@ -190,69 +187,7 @@ namespace Project
 
             
         
-        //static List<Employee> ProcessEmployeesFromXml(string filePath)
-        //{
-        //    XDocument doc = XDocument.Load(filePath);
-        //    var employees = doc.Descendants("Employee").Select(emp => new Employee
-        //    {
-        //        Id = emp.Attribute("employeeid")?.Value,
-        //        Name = emp.Element("Name")?.Value,
-        //        BirthYear = int.Parse(emp.Element("BirthYear")?.Value ?? "0"),
-        //        StartYear = int.Parse(emp.Element("StartYear")?.Value ?? "0"),
-        //        CompletedProjects = int.Parse(emp.Element("CompletedProjects")?.Value ?? "0"),
-        //        Active = bool.Parse(emp.Element("Active")?.Value ?? "false"),
-        //        Retired = bool.Parse(emp.Element("Retired")?.Value ?? "false"),
-        //        Email = emp.Element("Email")?.Value,
-        //        Phone = emp.Element("Phone")?.Value,
-        //        Job = emp.Element("Job")?.Value,
-        //        Level = emp.Element("Level")?.Value,
-        //        Salary = int.Parse(emp.Element("Salary")?.Value ?? "0"),
-        //        Commission = emp.Element("Commission")?.Attribute("currency")?.Value == "eur" ?
-        //            ConvertToHUF(decimal.Parse(emp.Element("Commission")?.Value ?? "0")) :
-        //            decimal.Parse(emp.Element("Commission")?.Value ?? "0"),
-        //        Departments = emp.Element("Departments")!.Elements("Department").Select(dept => new Department
-        //        {
-        //            Name = dept.Element("Name")!.Value,
-        //            DepartmentCode = dept.Element("DepartmentCode")!.Value,
-        //            HeadOfDepartment = dept.Element("HeadOfDepartment")!.Value
-        //        }).ToList()
-        //    }).ToList();
-
-        //    return employees;
-        //}
-        //private static void ImportXmlData()
-        //{
-
-        //    string xmlFilePath = "employees-departments.xml";
-        //    List<Employee> employees = ProcessEmployeesFromXml(xmlFilePath);
-
-        //    Console.WriteLine("XML adatok feldolgozva:");
-        //    foreach (var emp in employees)
-        //    {
-        //        Console.WriteLine($"Employee ID: {emp.Id}, Name: {emp.Name}, Salary: {emp.Salary} HUF");
-        //    }
-        //    Console.WriteLine("\nNyomj meg egy gombot a folytatáshoz...");
-        //    Console.ReadKey();
-        //}
-        //private static void ImportJsonData()
-        //{
-
-        //    string jsonFilePath = "managers.json"; 
-        //    List<Manager> managers = GetManagersFromFile(jsonFilePath);
-        //    Console.WriteLine("\nJSON adatok feldolgozva:");
-        //    foreach (var manager in managers)
-        //    {
-        //        Console.WriteLine($"ID: {manager.ManagerId}, Name: {manager.Name}, Start Year: {manager.StartOfEmployment.ToShortDateString()}, Has MBA: {manager.HasMBA}");
-        //    }
-        //    Console.WriteLine("\nNyomj meg egy gombot a folytatáshoz...");
-        //    Console.ReadKey(); 
-        //}
-        //private static List<Manager> GetManagersFromFile(string filePath)
-        //{
-        //    string jsonString = File.ReadAllText(filePath);
-        //    return JsonSerializer.Deserialize<List<Manager>>(jsonString);
-        //}
-
+        
         static decimal ConvertToHUF(decimal eurAmount)
         {
             decimal exchangeRate = 410;
